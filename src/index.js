15d8ca792f848848a44bd43c812ac407c6d7a3b0
Molski <macknull@gmail.com>
2019-11-01 14:45:48 +0100

    Add React, Babel, Webpack

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello Project.');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const title = 'React with Webpack and Babel.';
+ReactDOM.render(
+    <App title={title} />,
+    document.getElementById('app')
+);
+
+module.hot.accept();
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,29 @@
+const webpack = require('webpack');
+
 module.exports = {
-    // 1
     entry: './src/index.js',
-    // 2
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
     output: {
         path: __dirname + '/dist',
         publicPath: '/',
         filename: 'bundle.js'
     },
-    // 3
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        hot: true
     }
 };
