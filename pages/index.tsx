3d390e3cbbb6e2b5c773220672e7100dcded978e
maciej_molski_predict_systems <maciej.molski@predict.systems>
2022-06-10 00:53:34 +0200

    Fix flex wrap of herb content

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 
 const herbTraditionContent = 'W medycynie tradycyjnej  uchodził za panaceum. Tradycja stosowania rumianku w ziołolecznictwie sięga czasów najdawniejszych. Starożytni Egipcjanie uważali rumianek za dar Ra, boga Słońca i leczyli za jego pomocą oparzenia słoneczne. Dioskurides w „De Materia Medica” (50-70 r.n.e.) zalecał go jako środek na ból brzucha.'
 
-type HomeProps = {
+interface HomeProps {
   title: string, 
 }
 const Home: NextPage<HomeProps> = ({ title }) => {
@@ -29,38 +29,40 @@ const Home: NextPage<HomeProps> = ({ title }) => {
               <div className={styles['herb-name-latin-container']}>Matricaria chamomilla</div>
             </div>
           </div>
-          <div className={styles['herb-tradition-container']}>
-            <div className={styles['herb-tradition-content-container']}>
-              <div className={styles['herb-tradition-header-container']}>Tradycja</div>
-              <div className={styles['herb-tradition-subtext-container']}>
-                {herbTraditionContent}  
+          <div className={styles['herb-content-container']} >
+            <div className={styles['herb-tradition-container']}>
+              <div className={styles['herb-tradition-content-container']}>
+                <div className={styles['herb-tradition-header-container']}>Tradycja</div>
+                <div className={styles['herb-tradition-subtext-container']}>
+                  {herbTraditionContent}  
+                </div>
+              </div>
+              <div className={styles['tradition-icon-container']}>
+                <img className={styles['herb-content-icon']} src="/img/sun.jpg" alt='tradition icon' />
               </div>
             </div>
-            <div className={styles['tradition-icon-container']}>
-              <img className={styles['herb-content-icon']} src="/img/sun.jpg" alt='tradition icon' />
+            <div className={styles['herb-effects-container']}>
+              <div className={styles['herb-tradition-header-container']}>Działanie</div>
+              <div className={styles['herb-tradition-content-container']}>
+                <ul>
+                  <li>Wspomagające trawienie</li>
+                  <li>Przeciwzapalne</li>
+                  <li>Nasenne i uspokajające</li>
+                  <li>Wspomagające gojenie</li>
+                </ul>
+              </div>
             </div>
-          </div>
-          <div className={styles['herb-effects-container']}>
-            <div className={styles['herb-tradition-header-container']}>Działanie</div>
-            <div className={styles['herb-tradition-content-container']}>
-              <ul>
-                <li>Wspomagające trawienie</li>
-                <li>Przeciwzapalne</li>
-                <li>Nasenne i uspokajające</li>
-                <li>Wspomagające gojenie</li>
-              </ul>
-            </div>
-          </div>
-          <div className={styles['herbz-sections-spacing']}> </div>
-          <div className={styles['herb-effects-container']}>
-            <div className={styles['herb-tradition-header-container']}>Dolegliwości</div>
-            <div className={styles['herb-tradition-content-container']}>
-              <ul>
-                <li>Niestrawność</li>
-                <li>Infekcje</li>
-                <li>Stres</li>
-                <li>Lekkie urazy skóry</li>
-              </ul>
+            <div className={styles['herbz-sections-spacing']}> </div>
+            <div className={styles['herb-effects-container']}>
+              <div className={styles['herb-tradition-header-container']}>Dolegliwości</div>
+              <div className={styles['herb-tradition-content-container']}>
+                <ul>
+                  <li>Niestrawność</li>
+                  <li>Infekcje</li>
+                  <li>Stres</li>
+                  <li>Lekkie urazy skóry</li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
